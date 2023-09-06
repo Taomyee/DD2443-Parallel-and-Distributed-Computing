b08171df490934099de69cfcdb3f33bdc241f39a
YimingJu <yimingj@kth.se>
2023-09-06 10:55:26 +0200

    plot ex2_pdc

--- Lab1/lab1py/ex2.py
+++ Lab1/lab1py/ex2.py
@@ -10,3 +10,13 @@ plt.ylabel('Average execution time (ns)')
 plt.title('Average execution time vs. number of threads')
 plt.savefig('ex2.png')
 plt.show()
+
+avgExeTime_pdc = [452407,633392,522957,613013,652495,594005,633103]
+stdDeviation_pdc = [0.0,79227.0,68471.9,78530.9,26987.8,81735.8,73947.6]
+plt.plot(n, avgExeTime_pdc, 'ro')
+plt.errorbar(n, avgExeTime_pdc, yerr=stdDeviation_pdc, fmt='o')
+plt.xlabel('Number of threads')
+plt.ylabel('Average execution time (ns)')
+plt.title('Average execution time vs. number of threads (PDC)')
+plt.savefig('ex2_pdc.png')
+plt.show()
